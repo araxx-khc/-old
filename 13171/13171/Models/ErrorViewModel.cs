7c9d8b946a88e511a69486d262a165c5da9dbe71
araxx-khc <55738384+araxx-khc@users.noreply.github.com>
2022-02-02 11:29:24 +0100

    ‘13171-egzamin-asp.net’

---
+++ 13171/13171/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _13171.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
